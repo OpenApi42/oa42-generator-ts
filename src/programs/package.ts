8bc29a6d4ad8e8ebc8042e41e92c6a2ab64e8e53
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-10-20 18:06:24 +0200

    wip

--- src/programs/package.ts
+++ src/programs/package.ts
@@ -1,6 +1,10 @@
+import * as oas30 from "@jns42/jns42-schema-oas-v3-0";
+import * as oas31 from "@jns42/jns42-schema-oas-v3-1";
+import * as swagger2 from "@jns42/jns42-schema-swagger-v2";
+
 import * as path from "node:path";
-import ts from "typescript";
 import * as yargs from "yargs";
+import { loadYAML } from "../utils/load.js";
 
 export function configurePackageProgram(argv: yargs.Argv) {
   return argv.command(
@@ -53,6 +57,20 @@ async function main(options: MainOptions) {
 
   const packageDirectoryPath = path.resolve(options.packageDirectory);
   const { packageName, packageVersion, rootNamePart } = options;
+
+  const data = await loadYAML(specificationUrl);
+
+  if (swagger2.isSchemaJson(data)) {
+    console.log("swagger2");
+  }
+
+  if (oas30.isSchema20210928(data)) {
+    console.log("oas30");
+  }
+
+  if (oas31.isSchema20221007(data)) {
+    console.log("oas31");
+  }
 
   // todo
 }
