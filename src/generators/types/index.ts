38fb77364fa6f80b4a4903fc281ca1f31ae02414
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-11-06 16:04:55 +0100

    wip

--- src/generators/types/index.ts
+++ src/generators/types/index.ts
@@ -3,6 +3,7 @@ export * from "./operation-authentication.js";
 export * from "./operation-handler.js";
 export * from "./operation-incoming-request.js";
 export * from "./operation-outgoing-response.js";
-export * from "./parameters.js";
+export * from "./operation-parameters.js";
+export * from "./operation-result-parameters.js";
 export * from "./server-authentication.js";
 export * from "./server.js";
--- src/generators/types/operation-result-parameters.ts
+++ src/generators/types/operation-result-parameters.ts
@@ -3,41 +3,6 @@ import * as models from "../../models/index.js";
 import { itt } from "../../utils/iterable-text-template.js";
 import { toPascal } from "../../utils/name.js";
 
-export function* generateOperationParametersTypes(
-  apiModel: models.Api,
-  operationModel: models.Operation,
-) {
-  const operationRequestParametersName = toPascal(
-    operationModel.name,
-    "request",
-    "parameters",
-  );
-
-  const allParameterModels = [
-    ...operationModel.queryParameters,
-    ...operationModel.headerParameters,
-    ...operationModel.pathParameters,
-    ...operationModel.cookieParameters,
-  ];
-
-  yield itt`
-    export type ${operationRequestParametersName} = {
-      ${allParameterModels.map((parameterModel) => {
-        const parameterSchemaId = parameterModel.schemaId;
-        const parameterTypeName =
-          parameterSchemaId == null
-            ? parameterSchemaId
-            : apiModel.names[parameterSchemaId];
-
-        return itt`
-    ${camelcase(parameterModel.name)}${parameterModel.required ? "?" : ""}:
-      ${parameterTypeName == null ? "unknown" : parameterTypeName}
-    `;
-      })}
-    };
-  `;
-}
-
 export function* generateOperationResultParameterTypes(
   apiModel: models.Api,
   operationModel: models.Operation,
